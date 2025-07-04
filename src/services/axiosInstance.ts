7fb2435a146ffc0c03c4313ee69c27c58b296c84
Thông <thongtase171619@gmail.com>
2025-07-04 13:50:38 +0700

    Update axiosInstance.ts

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: import.meta.env.VITE_REACT_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
